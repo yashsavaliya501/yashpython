0542acca038914d6f1c1369b48e8ee9b46ef9a79
yashsavaliya501 <yashsavaliya501@gmail.com>
2023-04-05 17:27:47 +0530

    update

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
-
+from fp.fp import FreeProxy
+import json
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    proxy = FreeProxy(country_id=['US','UK'], timeout=1, rand=True, anonym=True, https=True).get()
+    var22 = proxy.split('/')
+    myList = {'ipPort': var22[2], 'country': 'US', 'type': 'http'}
+    jsonString = json.dumps(myList, indent=4)
+    return jsonString
 
 if __name__ == '__main__':
     app.run()
